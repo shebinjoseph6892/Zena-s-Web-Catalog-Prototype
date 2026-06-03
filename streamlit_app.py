6293ffcdb01c1eb95a9e3debe34a51275fb0e05d
shebinjoseph6892 <116418010+shebinjoseph6892@users.noreply.github.com>
2026-06-03 20:17:47 +0530

    Refactor smoothie app to display athleisure catalog

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,36 @@
-# Import python packages
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import pandas as pd
 
-# Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
-st.write(
-  """Choose the Fruits you want in your custom Smoothie!
-  """
-)
-Name_on_order = st.text_input('Name on Smoothie:')
-st.write('The Name on smoothie will be:', Name_on_order)
+st.title("Zena's Amazing Athleisure Catalog")
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select (col("FRUIT_NAME"))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-ingredients_list= st.multiselect ('Choose up to 5 ingredients:', my_dataframe )
+#cnx = st.connection("snowflake")
+#session = cnx.session()
 
-if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
-    ingredients_string = ''
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                    values ('""" + ingredients_string + """','""" + Name_on_order + """')"""
+# get a list of colors for a drop list selection
+table_colors = session.sql("select color_or_style from zenas_athleisure_db.products.catalog_for_website")
+pd_colors = table_colors.to_pandas()
 
-   # st.write(my_insert_stmt)
-   # st.stop()
-    time_to_insert = st.button('Submit Order')
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success(f'Your Smoothie is ordered, {Name_on_order}', icon="✅")
+# Oyt the list of colors into a drop list selector 
+option = st.selectbox('Pick a sweatsuit color or style:', pd_colors)
+
+# We'll build the image caption now, since we can
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+
+# use the color selected to go back and get all the info from the database
+table_prod_data = session.sql("select file_name, price, size_list, upsell_product_desc, file_url from zenas_athleisure_db.products.catalog_for_website where color_or_style = '" + option + "';")
+pd_prod_data = table_prod_data.to_pandas() 
+
+# assign each column of the row returned to its own variable 
+price = '$' + str(pd_prod_data['PRICE'].iloc[0])+'0'
+file_name = pd_prod_data['FILE_NAME'].iloc[0]
+size_list = pd_prod_data['SIZE_LIST'].iloc[0]
+upsell = pd_prod_data['UPSELL_PRODUCT_DESC'].iloc[0]
+url = pd_prod_data['FILE_URL'].iloc[0]
+
+# display the info on the page
+st.image(image=url, width=400, caption=product_caption)
+st.markdown('**Price:** '+ price)
+st.markdown('**Sizes Available:** ' + size_list)
+st.markdown('**Also Consider:** ' + upsell)
